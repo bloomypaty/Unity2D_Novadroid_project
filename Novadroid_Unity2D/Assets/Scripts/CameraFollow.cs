875a8c0617e52225a0e1747b258e46dcf474798f
bloomypaty <skrzynskapat@gmail.com>
2017-11-26 14:33:39 +0100

    Unity2D project added (only Unity files)

---
+++ Novadroid_Unity2D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    /*private Vector2 velocity;
+    public float smoothTimeY;
+    public float smoothTimeX;
+    public GameObject player;
+
+    void Start () {
+
+        player = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+    void FIdesUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }*/
+    //[SerializeField] = To make the private variable visible in the inspector.
+    //private float xMax;
+  
+    private Transform target;
+    public float yOffset;
+
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        //Vector3(0,0,0)
+        transform.position = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
+    }
+}
---
+++ Novadroid_Unity2D/Assets/Scripts/Walking.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour {
+
+    public int playerSpeed = 10;
+    public int playerJumpPower = 10;
+
+    private bool facingRight = false;
+    private float moveX;
+    private float jumpY;
+
+
+    //private Rigidbody2D rigid;
+
+    void Start (){
+        
+    }
+	
+	
+	void Update (){
+        float move = Input.GetAxis("Horizontal");
+        
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    rigid.velocity = new Vector2(move * speed, rigid.velocity.y);
+        //}
+
+        PlayerMove();
+    }
+    //Main player movements
+    void PlayerMove(){
+        //CONTROLS
+        moveX = Input.GetAxis("Horizontal");
+        //"Jump" = spacebar
+        if (Input.GetButtonDown("Jump")){
+            Jump();
+        }
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        if (moveX < 0.0f && facingRight == false){
+            FlipPlayer();
+        } else if (moveX > 0.0f && facingRight == true){
+            FlipPlayer();
+        }
+
+        //PHYSICS
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+    void Jump(){
+        //Accessing the Rigidbody 2D
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpPower);
+    }
+
+    void FlipPlayer(){
+        facingRight = !facingRight;
+        //PLAYER SCALE
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+   
+    }
+}
